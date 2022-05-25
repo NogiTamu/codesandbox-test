8ea2dae96db8791a69aed67c1274245050b7f580
NogiTamu <76116273+NogiTamu@users.noreply.github.com>
2022-05-25 23:54:27 +0900

    メソッド

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import "./styles.css";
+// const name = "Mike";
+// const age = 28;
+// const massage1 = "私の名前は" + name + "です" + age + "です";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// console.log(massage1);
+
+// const message2 = `私の名前は${name}です。年齢は${age}`;
+// console.log(message2);
+
+const nameArr = ["田中", "山田", "アホ"];
+
+// for(let i=0;i<nameArr.length;i++ ){
+//   console.log(nameArr[i])
+// }
+
+const Arr2 = nameArr.map((name) => {
+  return name + "j";
+});
+
+// console.log(Arr2)
+
+nameArr.map((name, i) => {
+  console.log(`${i}番目は${name}`);
+});
+
+const numArr = [1, 2, 3, 4];
+
+const newnumArr = numArr.filter((num) => {
+  return num % 2 === 1;
+});
+console.log(newnumArr);
+
+const Arr3 = nameArr.map((name) => {
+  if (name === "アホ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(Arr3);
